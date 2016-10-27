68faf1baa374ca5c162a5b7e6133624883ac46c9
aehmt <calisahmet@gmail.com>
2016-10-27 15:33:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+  time: Math.round(Date.now()/1000),
+  startTicking: function() {setInterval(() => this.time++, 1000)}
+}
+
+const censor = function(word, text) {
+  let string = text.split(" ");
+  for (var i = 0, len = string.length; i < len; i++) {
+    if (string[i].includes(word)) {
+      let tmp = string[i].replace(word, "BLEEP")
+      string[i] = tmp
+    }
+  }
+  return string.join(" ")
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor = censor.bind(null, 'drugs');
